7d85e1ad7a320879061fd2107af204a73973fe91
asaad makhdoom <asad.makhdoom7@gmail.com>
2021-07-01 13:37:34 +0500

    Updated gitignore

--- server.js
+++ server.js
@@ -24,13 +24,14 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 
 // for production
-// if(process.env.NODE_ENV === "production"){
-//     app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-//     app.get("*", (req, res)=>{
-//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-//     })
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
